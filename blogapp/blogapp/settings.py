850ab2cc88ef245aac616ebcd7047c9076636fb2
MehmetKaan96 <mehmetkaan1996@gmail.com>
2022-05-18 14:16:11 +0300

    key change

--- blogapp/blogapp/settings.py
+++ blogapp/blogapp/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nd=gldgucqq)gp-g)+3qeu*xm5$8f)$7vwm-7pi6l4mh+6fevw'
+SECRET_KEY = ''
+#key:django-insecure-nd=gldgucqq)gp-g)+3qeu*xm5$8f)$7vwm-7pi6l4mh+6fevw
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
